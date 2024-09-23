25b2a2255fa798dd00ed44921ad39908dd0fe8a2
yuudachi13 <t2pupa00@students.oamk.fi>
2024-09-23 15:09:49 +0300

    pystyy lisäämään markkerin mappiin

--- screens/Map.js
+++ screens/Map.js
@@ -1,23 +1,37 @@
 import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import * as Location from 'expo-location';
 
 export default function Map(props) {
-
+    const [marker, setMarker] = useState(null)
     /*useEffect(() => {
         (async () => {
             getUserPosition()
         })()
     }, [])
  */
+
+    const showMarker = (e) => {
+        const coords = e.nativeEvent.coordinate
+        setMarker(coords)
+    }
     return (
         <MapView
             style={styles.map}
             region={props.location}
            // mapType="satellite"  //muuttaa tyypin sateliittinäkymäksi
-        />
-    )
+           onLongPress={showMarker}
+        >
+            {marker &&
+            <Marker
+            title="my marker"
+            coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
+            />
+            }
+            </MapView>
+            
+        )
 }
 
 const styles = StyleSheet.create({
