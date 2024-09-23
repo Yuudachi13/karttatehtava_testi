be5749c9ad559e60d9ba53e7fb1e277f427fee05
yuudachi13 <t2pupa00@students.oamk.fi>
2024-09-23 15:00:29 +0300

    nyt voidaan nappia painamalla saaha gps sijainti

--- App.js
+++ App.js
@@ -1,12 +1,60 @@
 import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Map from './screens/Map';
 import Constants from 'expo-constants';
+import { useState } from 'react';
+import { PaperProvider } from 'react-native-paper';
+import MainAppBar from './components/MainAppBar';
+import * as Location from 'expo-location';
+
+
+const settings = {
+  backgroundColor: '#00a484'
+}
+const icons = {
+  location_not_known: 'crosshairs',
+  location_searching: 'crosshairs-question',
+  location_found: 'crosshairs-gps'
+}
 
 export default function App() {
+  const [icon, setIcon] = useState(icons.location_not_known)
+  const [location, setLocation] = useState({
+    latitude: 65.0800,
+    longitude: 25.4800,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421
+})
+
+
+
+const getUserPosition = async () => {
+  setIcon(icons.location_searching)
+  let { status } = await Location.requestForegroundPermissionsAsync()
+
+  try {
+      if (status !== 'granted') {
+          console.log('Geolocatio failed')
+          return
+      }
+      const position = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High })
+      setLocation({ ...location, "latitude": position.coords.latitude, "longitude": position.coords.longitude })
+      setIcon(icons.location_found)
+  } catch (error) {
+      console.log(error)
+  }
+}
   return (
+    <PaperProvider>
+      <MainAppBar
+        title='Map'
+        backgroundColor={settings.backgroundColor}
+        icon={icon}
+        getUserPosition={getUserPosition}
+      />
     <SafeAreaView style={styles.container}>
-      <Map />
+      <Map location={location} />
     </SafeAreaView> 
+    </PaperProvider>
   )
 }
 
--- screens/Map.js
+++ screens/Map.js
@@ -3,39 +3,19 @@ import { StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 import * as Location from 'expo-location';
 
-export default function Map() {
-    const [location, setLocation] = useState({
-        latitude: 65.0800,
-        longitude: 25.4800,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-    })
+export default function Map(props) {
 
-    const getUserPosition = async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync()
-
-        try {
-            if (status !== 'granted') {
-                console.log('Geolocatio failed')
-                return
-            }
-            const position = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High })
-            setLocation({ ...location, "latitude": position.coords.latitude, "longitude": position.coords.longitude })
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    useEffect(() => {
+    /*useEffect(() => {
         (async () => {
             getUserPosition()
         })()
     }, [])
-
+ */
     return (
         <MapView
             style={styles.map}
-            region={location}
+            region={props.location}
+           // mapType="satellite"  //muuttaa tyypin sateliittinäkymäksi
         />
     )
 }
